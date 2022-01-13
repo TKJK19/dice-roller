25d783de6723cb1079497b7eac5299a9dcaa7c59
Tyler Kraft <86270945+TKJK19@users.noreply.github.com>
2022-01-13 10:16:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const rollDice = () => {
   let dieOne = document.querySelector('#die_1');
   let dieTwo = document.querySelector('#die_2');
   let winner = document.querySelector('#winner');
-  dieOne.src = `/assets/dice-${rolls[0]}.jpg`;
-  dieTwo.src = `/assets/dice-${rolls[1]}.jpg`;
+  dieOne.src = `assets/dice-${rolls[0]}.jpg`;
+  dieTwo.src = `assets/dice-${rolls[1]}.jpg`;
   winner.innerText = rolls[0] === rolls[1] ? 'Draw!' : rolls[0] > rolls[1] ? 'Player 1 wins!' : 'Player 2 wins!';
   console.dir(`Player one is ${rolls[0]}`);
   console.dir(`Player two is ${rolls[1]}`);
